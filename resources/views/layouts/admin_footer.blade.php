98b6990ff66726953d4f6cd24e99ecc4101068ff
blade1996 <iesus10@hotmail.com>
2021-12-28 20:18:44 -0500

    feat:(COURSE) - Mostrar Cantidad de Retos Registrados 2

--- resources/views/layouts/admin_footer.blade.php
+++ resources/views/layouts/admin_footer.blade.php
@@ -1,6 +1,6 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-  <strong>Copyright &copy; 2020 <a href="http://adminlte.io">Enel</a>.</strong>
-  <div class="float-right d-none d-sm-inline-block">
-  </div>
+    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">XH</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+    </div>
 </footer>
