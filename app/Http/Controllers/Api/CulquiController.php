147bd728277454c64e7b251763d33d2fc202e05d
blade1996 <iesus10@hotmail.com>
2022-01-07 00:34:14 -0500

    feat:(CULQUI) - Manejo de Excepciones

--- app/Http/Controllers/Api/CulquiController.php
+++ app/Http/Controllers/Api/CulquiController.php
@@ -4,23 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Http\Request;
 
 class CulquiController extends Controller
 {
     public function createCharge(Request $request)
     {
-
-        $data = $request->all();
-        $client = new Client(['base_uri' => env('CULQUI_API_URL')]);
-        $options = [
-            'headers' => [
-                'authorization' => 'Bearer ' . env('API_KEY_CULQUI'),
-                'Content-Type' => 'application/json'
-            ],
-            'json' => $data,
-        ];
-        $response = $client->request('POST', 'charges', $options);
-        return $response->getBody()->getContents();
+        try {
+            $data = $request->all();
+            $client = new Client(['base_uri' => env('CULQUI_API_URL')]);
+            $options = [
+                'headers' => [
+                    'authorization' => 'Bearer ' . env('API_KEY_CULQUI'),
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => $data,
+            ];
+            $response = $client->request('POST', 'charges', $options);
+            return $response->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            return $e->getResponse();
+        }
     }
 }
