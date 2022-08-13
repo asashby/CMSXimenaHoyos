45f0f7c05da0ade9f75d2246e06107bb0e2fee4e
DTK <cristhiandanielcoaquira@gmail.com>
2022-08-12 19:32:02 -0500

    Implement soft deletes in products

--- app/Product.php
+++ app/Product.php
@@ -3,13 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
 class Product extends Model implements HasMedia
 {
-
-    use HasMediaTrait;
+    use HasMediaTrait, SoftDeletes;
 
     protected $fillable = ["name", "slug", "attributes", "description", "price", "url_image", "sku"];
 
